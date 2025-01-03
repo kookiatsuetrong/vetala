00f4c129ac5914a8015bb413323324e0b95066eb
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2025-01-03 16:11:30 +0700

    Fix make.sh for macOS

--- make.sh
+++ make.sh
@@ -1,8 +1,11 @@
 javac -classpath "runtime/*" vetala/Handler.java
 javac -classpath "runtime/*:vetala" vetala/*.java
 javac -classpath "runtime/*:vetala" vetala/server/*.java
+
 rm runtime/*.class
 mv vetala/*.class runtime
+
 rm runtime/server/*.class
 mv vetala/server/*.class runtime/server
+
 java -classpath "runtime/*:runtime" Vetala
